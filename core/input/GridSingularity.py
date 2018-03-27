10dfbb7b4408bb4ea4c0671182717b3285a6f6c7
ewf-updater <ewf-updater@ewf.org>
2018-03-27 12:07:26 +0200

    added GridSing consumption interface prototype

--- core/input/GridSingularity.py
+++ core/input/GridSingularity.py
@@ -5,31 +5,26 @@ import datetime
 
 from core.abstract.input import ExternalDataSource, EnergyData, Device
 
-# consuming asset
-class SPGroupAPI(ExternalDataSource):
+# consumption asset
+class GridSingularity(ExternalDataSource):
 
     def __init__(self, site_id: str):
 
         self.site = site_id
-        self.api_url = 'https://lighthouse-api-master.apps.vpcf-qa.spdigital.io/v1/'
+        self.api_url = 'https://app1pub.smappee.net/dev/v1/servicelocation/'
 
     def read_state(self) -> EnergyData:
         raw = self._get_daily_data()
         '''
-        {
-            "sites": [
-                {
-                    "site_id": "b1",
-                    "start_time": "2018-03-26T08:21:20Z",
-                    "end_time": "2018-03-26T09:21:20Z",
-                    "energy": {
-                        "unit": "wh",
-                        "data": 875.4090909090909
-                    }
-                },
-                ...
-            ]
-        }
+            {
+            "serviceLocationId": 123,
+            “consumptions” : [ 
+                { “timestamp”: 1372672500000, “consumption”: 23, “solar”: 56, “alwaysOn”: 12 },
+                { “timestamp”: 1372672800000, “consumption”: 67, “solar”: 57, “alwaysOn”: 12 },
+                { “timestamp”: 1372673100000, “consumption”: 88, “solar”: 58, “alwaysOn”: 12 },
+                ...                 
+              ] 
+            }
         '''
         # get the object with the right site_id
         state = {}
@@ -49,7 +44,6 @@ class SPGroupAPI(ExternalDataSource):
         device = Device(**device_meta)
 
         # get produced energy from filtered object
-        # accumulated_power = specific_site['energy']['data']
         accumulated_power = int(("%.2f" % specific_site['energy']['data']).replace('.', ''))
 
         # build access_epoch
@@ -63,29 +57,38 @@ class SPGroupAPI(ExternalDataSource):
         return EnergyData(device, access_epoch, raw, accumulated_power, measurement_epoch)
 
     def _get_daily_data(self) -> dict:
-        marginal_query = {
-            'limit': 5,
-            'start': 'last_hour',
-            'end': 'now'
-        }
-        # sending header until we get usr pwd
-        provisional_header = {"Authorization":"Basic ZGdmanNkamoyMzIzMjM5ODc5ZGtma2gzZWhmazM6OTg3OTBpa2pmZGtmM2hrMmpoZGtqaGkza2RzYjM="}
-        endpoint = self.api_url + 'produced'
+        # calculate the current time and one hour back from there
+        d = datetime.datetime.utcnow()
+        epoch = datetime.datetime(1970, 1, 1)
+        time_now = (d - epoch).total_seconds()
+        time_one_hour_ago = time_now - 3600
 
-        r = requests.get(endpoint, params=marginal_query, headers=provisional_header, verify=False)
+        marginal_query = {
+            'aggregation': 2,
+            'from': time_one_hour_ago,
+            'to': time_now
+        }
+
+        # build the endpoint for the request
+        endpoint = self.api_url + self.site_id + '/consumption/'
+
+        # do the access management (get access token)
+        # TODO: ask jens about that junk
+
+        # start request
+        r = requests.get(endpoint, params=marginal_query, verify=False)
         ans = r.json()
-        if len(ans['sites']) < 1:
+        if len(ans['serviceLocationId']) < 1:
             raise AttributeError('Empty response from api.')
         return ans
 
 
-# needs a site_id
-class SPGroup_b1(SPGroupAPI):
+class GridSingularity_123(GridSingularity):
 
-    def __init__(self, site_id: 'b1'):
+    def __init__(self, site_id: '123'):
         super().__init__(site_id)
 
 
 if __name__ == '__main__':
-    sp = SPGroup_b1('b1')
-    sp.read_state()
+    gs = GridSingularity_123('123')
+    gs.read_state()
