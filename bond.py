5f3357a4c4a83f061096d207a2d782a714c7455b
cerealkill <cerealkill@users.noreply.github.com>
2018-04-09 15:10:53 +0200

    Strict config load

--- bond.py
+++ bond.py
@@ -30,11 +30,11 @@ if __name__ == '__main__':
     print(os.getcwd())
     print(os.listdir(os.getcwd()))
 
-    subprocess.Popen(["./assets/ewf-client-arm", "--jsonrpc-apis", "all", "--reserved-peers", "./tobalaba-peers"],
-                     # stdout=subprocess.PIPE,
-                     # stderr=subprocess.PIPE
-                     )
-    print('waiting for ewf-client...\n\n')
+    # subprocess.Popen(["./assets/ewf-client-arm", "--jsonrpc-apis", "all", "--reserved-peers", "./tobalaba-peers"],
+    #                  # stdout=subprocess.PIPE,
+    #                  # stderr=subprocess.PIPE
+    #                  )
+    # print('waiting for ewf-client...\n\n')
 
     # while infinite:
     print('`•.,,.•´¯¯`•.,,.•´¯¯`•.,, Config ,,.•´¯¯`•.,,.•´¯¯`•.,,.•´\n')
