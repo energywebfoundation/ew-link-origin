d8b95de000509e73ebb3b3daf57bf313eb12675d
cerealkill <cerealkill@users.noreply.github.com>
2018-06-04 14:12:54 +0200

    More reorg

--- bond/certificate_of_origin/migrations/1_initial_migration.js
+++
@@ -1,5 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations, { gasPrice: 0, from: "0x3B07F15EFb10f29B3fC222fb7E717e9Af0cC4243" });
-};
