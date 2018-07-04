2166d0e6cb99616c83ce2b1226a346a5244c1224
cerealkill <cerealkill@users.noreply.github.com>
2018-07-04 13:03:26 +0100

    Further explanation and local configuration

---
+++ bond/local-producer.py
@@ -0,0 +1,24 @@
+import subprocess
+import time
+
+import core.helper as core
+
+JSON = 'assets/local-producer.json'
+
+
+# Helper to spawn an Tobalaba client on execution
+def start_ewf_client():
+    subprocess.Popen(["/usr/local/bin/ewf-client", "--jsonrpc-apis", "all", "--reserved-peers",
+                      "/assets/tobalaba-peers"], stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+    print('waiting for ewf-client...\n\n')
+    time.sleep(60)
+
+
+# The original bond code is executed on a scheduled basis and connects to all producers and consumers listed on
+# the json file. After gathering data on the api it signs a transaction to a smart-contract and sends it
+# to the client also specified on the json file.
+# This method jumps the scheduling process, logs the data on time of execution and exits.
+if __name__ == '__main__':
+    configuration = core.print_config(JSON)
+    core.log(configuration)
