7564556510261f45d3923e031c85c647cc8df520
cerealkill <cerealkill@users.noreply.github.com>
2018-09-17 16:39:39 +0200

    Adding bond dependencies. - Automatic commit.

--- sendToResin.sh
+++ sendToResin.sh
@@ -1,4 +1,9 @@
 git submodule update --recursive --remote
+rm -rf ./consumer/bond
+rm -rf ./producer/bond
+git add ./consumer/bond
+git add ./producer/bond
+git commit -am 'Adding bond dependencies. - Automatic commit.'
 cp -r -v ./bond ./producer/bond
 cp -r -v ./bond ./consumer/bond
 git push
